5e340307d0aa93b52025dee2ff06cad42d45a532
bibek77 <bibekrana01@gmail.com>
2023-10-18 21:02:01 +0530

    internationalization

--- src/main/java/com/udemycourse/springboot/learnspringbootjpa/controller/HelloWorldController.java
+++ src/main/java/com/udemycourse/springboot/learnspringbootjpa/controller/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.udemycourse.springboot.learnspringbootjpa.controller;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 /**
  * @author bibek
  */
-//@RestController
+@RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello")
 //    @RequestMapping(method= RequestMethod.GET, path = "/hello")
@@ -22,6 +32,13 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorld(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world/internationalization")
+    public String helloWordInternationalization() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+//        return "Hello Word v2";
     }
 
 }
--- src/main/java/com/udemycourse/springboot/learnspringbootjpa/user/UserResource.java
+++ src/main/java/com/udemycourse/springboot/learnspringbootjpa/user/UserResource.java
@@ -2,6 +2,7 @@ package com.udemycourse.springboot.learnspringbootjpa.user;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
